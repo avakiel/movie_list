7739ffd2dece6bb975ed67e09cf8c0f3170b3c91
avakiel <79792776+avakiel@users.noreply.github.com>
2024-04-17 16:08:37 +0300

    fix route

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import store from "./Redux/Store";
 import { Provider } from "react-redux";
 
